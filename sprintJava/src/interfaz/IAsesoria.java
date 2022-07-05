6d718b14d50e0f0f2a0455b1e0d6a087bd661d90
Amyyinthesky <104643206+Amyyinthesky@users.noreply.github.com>
2022-07-05 13:27:55 -0400

    Delete sprintJava directory

--- sprintJava/src/interfaz/IAsesoria.java
+++
@@ -1,9 +0,0 @@
-package interfaz;
-
-/**
- * Interfaz encargada de analizar datos de "Usuario" solicitados por las distintas Clases de este Tipo.
- */
-
-public interface IAsesoria {
-    public String analizarUsuario();
-}
